286f66c1b6053174cd754a0e1b472a0ddb4803d6
Dhanish kumar <dhanishmca1@gmail.com>
2025-01-08 12:15:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your code here
+cy.get('ol li:last audio')
+  .should('have.attr', 'src', 'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg')
+  .and('have.attr', 'controls');  // Ensure the audio element has controls
